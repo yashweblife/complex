098ba716506debdc2a48a9afcc8388c7da26cae8
giriyg <ygg3y8@missouri.edu>
2024-02-05 09:40:32 -0600

    add box to library

---
+++ src/lib/Box/Box.ts
@@ -0,0 +1,10 @@
+import { Vector } from "..";
+
+export class Box{
+    public pos:Vector;
+    public vel:Vector;
+    public acc:Vector;
+    public size:Vector;
+    constructor(){}
+    update(){}
+}
