bcd0f029b69745c08957fc9e740ddfc4995681bc
giriyg <ygg3y8@missouri.edu>
2024-02-05 09:36:06 -0600

    add Ball methods and stuff

--- src/lib/Ball/Ball.ts
+++ src/lib/Ball/Ball.ts
@@ -1,1 +1,48 @@
-export class Ball{}
+import { Box, Canvas, Vector } from "..";
+
+export class Ball{
+    public pos:Vector;
+    public vel:Vector;
+    public acc:Vector;
+    public r:Vector;
+    public color:Vector;
+    public forces:Vector[];
+    
+    constructor(){
+
+    }
+    /**
+     * Repel the ball from a given vector
+     * @param v Vector
+     */
+    repel(v:Vector){}
+    /**
+     * Attract the ball to a given vector
+     * @param v Vector
+     */
+    attract(v:Vector){}
+    /**
+     * Check collision with another ball
+     * @param b Ball
+     */
+    checkCollisionWithBall(b:Ball){}
+    /**
+     * Check collision with another box
+     * @param b Box
+     */
+    checkCollisionWithBox(b:Box){}
+    /**
+     * Draw the ball
+     * @param c Canvas
+     */
+    draw(c:Canvas){}
+    /**
+     * Add forces to the ball;
+     * @param v Vector
+     */
+    addForce(v:Vector){}
+    /**
+     * Update the balls physics
+     */
+    update(){}
+}
