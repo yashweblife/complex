0d9cc8b57e05367f22f4898f1db124eb5cbdcf3a
giriyg <ygg3y8@missouri.edu>
2024-02-05 09:42:34 -0600

    add pipes for new library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 import { Vector } from "./Vector/Vector";
-
-export {Vector}
+import { Ball } from "./Ball/Ball";
+import { Canvas } from "./Canvas/Canvas";
+import { Box } from "./Box/Box";
+export {Vector, Ball, Canvas, Box}
